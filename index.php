0ebd740cf754499e75ddf79fe5f3f84c6a864945
Victor López <joselopez16_99@outlook.com>
2021-06-11 20:15:33 -0600

    Agregando archivos PHP

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Document</title>
+	</head>
+	<body>
+		<h2><?php echo "Hola mundo!"; ?></h2>
+	</body>
+</html>
