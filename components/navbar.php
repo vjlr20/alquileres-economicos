63a095eb499fa08baa2b746468c23aee37aa1cfa
Victor5032 <victorlopez_20@outlook.com>
2021-06-27 17:04:37 -0600

    Navbar completo

--- components/navbar.php
+++ components/navbar.php
@@ -12,15 +12,6 @@
 						<li><a class="dropdown-item" href="index.php?page=nuevo-cliente">Nuevo cliente</a></li>
 					</ul>
 				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Alquileres</a>
-					<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-						<li><a class="dropdown-item" href="#">Action</a></li>
-						<li><a class="dropdown-item" href="#">Another action</a></li>
-						<li><hr class="dropdown-divider"></li>
-						<li><a class="dropdown-item" href="#">Something else here</a></li>
-					</ul>
-				</li>
 			</ul>
 			<div class="d-flex">
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
